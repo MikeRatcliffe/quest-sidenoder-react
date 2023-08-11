97d315688350f5ffe90132387caeabdee0d6a9b8
Mike Ratcliffe <michael@ratcliffefamily.org>
2023-08-11 21:33:03 +0100

    fix: Add icons and begin web content

--- src/renderer/App.js
+++ src/renderer/App.js
@@ -1,40 +1,14 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
+import { Icon } from '../../assets/Icon.jsx';
 import './App.css';
 
 function Hello() {
   return (
     <div>
-      <div className="Hello">
-        <img width="200" alt="icon" src={icon} />
+      <div className="app-container">
+        <Icon width={200} height={200} title="Quest Sidenoder" />
       </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="folded hands">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
+      <h1>Quest Sidenoder</h1>
     </div>
   );
 }
